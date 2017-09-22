893f25c7fa6d2f63abcc05fb4dd9a2e6b17dce09
SK <Techifynow@192.168.1.3>
2017-09-22 09:38:31 +0530

    Package creation for team

---
+++ src/com/bizvalue/siva/HellowWorld.java
@@ -0,0 +1,10 @@
+package com.bizvalue.siva;
+
+public class HellowWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Step1");
+	}
+
+}
---
+++ src/com/bizvalue/srikanth/HellowWorld.java
@@ -0,0 +1,10 @@
+package com.bizvalue.srikanth;
+
+public class HellowWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Step1");
+	}
+
+}
